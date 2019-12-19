080a81d8b4e23ad13f235951f1eaf0aa2f95db63
osdajigu <odjimenezg@unal.edu.co>
2019-12-19 14:34:50 -0500

    updated regional 2019

--- World Final Notebook/Data structures/Order statistics.cpp
+++
@@ -1,5 +0,0 @@
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-typedef tree<int, null_type, less<int>, rb_tree_tag,
-tree_order_statistics_node_update> ordered_set;
--- World Final Notebook/Utilities/Pragma optimizations.cpp
+++
@@ -1,2 +0,0 @@
-#pragma GCC optimize ("O3") #pragma GCC target ("sse4")
-#pragma GCC target ("avx,tune=native")
