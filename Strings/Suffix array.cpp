8435f674a9f63b6cf275697aff44219f24fd3030
osdajigu <odjimenezg@unal.edu.co>
2020-06-03 17:05:29 -0500

    changed LCP for i, i+1

--- Strings/Suffix array.cpp
+++ Strings/Suffix array.cpp
@@ -25,8 +25,9 @@ struct suffix_array { // s MUST not have 0 value
     }
     swap(sa, a);
     for(int i = 0; i < n; i++) pos[ sa[i] ] = i;
-    for(int i = 0, k = 0, j; i < n-1; lcp[ pos[i++] ] = k) {
-      for(k = max(0, k-1), j = sa[ pos[i]-1 ]; s[i+k] == s[j+k]; k++);
+    for(int i = 0, k = 0, j; i < n; lcp[ pos[i++] ] = k) {
+      if(pos[i] == n-1) continue;
+      for(k = max(0, k-1), j = sa[ pos[i]+1 ]; s[i+k] == s[j+k]; k++);
     }
   }
   int& operator[] ( int i ){ return sa[i]; }
