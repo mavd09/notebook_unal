59a5d8e33c31b196562adef378250aa5252bb990
osdajigu <odjimenezg@unal.edu.co>
2020-06-08 16:46:13 -0500

    add eulerian for undirected graphs

--- Graphs/Eulerian path.cpp
+++ Graphs/Eulerian path.cpp
@@ -1,5 +1,15 @@
 /// Complexity: O(|N|)
 /// Tested: https://tinyurl.com/y85t8e83
+struct edge {
+  int v; //list<edge>::iterator rev;
+  edge(int v) : v(v) {}
+};
+void add_edge(int a, int b) {
+  g[a].push_front(edge(b)); // auto ia = g[a].begin();
+	g[b].push_front(edge(a)); // auto ib = g[b].begin();
+	//ia->rev=ib; ib->rev=ia;
+}
+/// for undirected uncomment and check for path existance
 bool eulerian(vector<int> &tour) { /// directed graph
   int one_in = 0, one_out = 0, start = -1;
   bool ok = true;
@@ -13,9 +23,10 @@ bool eulerian(vector<int> &tour) { /// directed graph
   if (ok) {
     function<void(int)> go = [&](int u) {
       while(g[u].size()) {
-        int v = g[u].back();
-        g[u].pop_back();
-        go(v);
+        int v = g[u].front().v;
+        g[v].erase(g[u].front().rev);
+        g[u].pop_front();
+        go(v, tour);
       }
       tour.push_back(u);
     };
