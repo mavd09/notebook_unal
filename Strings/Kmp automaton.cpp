d46ba3a751d515c7b1e2f7cf0ad7b5451c4a5fe7
osdajigu <odjimenezg@unal.edu.co>
2020-11-09 20:26:51 -0500

    Add shorter kmp automata

--- Strings/Kmp automaton.cpp
+++ Strings/Kmp automaton.cpp
@@ -1,13 +1,14 @@
 /// Complexity: O(|N|*alphabet)
 /// Tested: not yet
 const int alpha = 256;
-int aut[102][alpha];
+int aut[N][alpha];
 void kmp_automaton(string &t) {
-  vector<int> phi = get_phi(t);
-  for(int i = 0; i <= t.size(); ++i) {
-    for(int c = 0; c < alpha; ++c) {
-      if(i == t.size() || (i > 0 && c != t[i])) aut[i][c] = aut[ phi[i-1] ][c];
-      else aut[i][c] = i + (c == t[i]);
+  aut[0][ t[0] ] = 1;
+  for(int i = 1, j = 0; i <= t.size(); ++i) {
+    for(int c = 0; c < alpha; ++c) aut[i][c] = aut[j][c];
+    if(i < t.size()) {
+      aut[i][ t[i] ] = i+1;
+      j = aut[j][ t[i] ];
     }
   }
 }
